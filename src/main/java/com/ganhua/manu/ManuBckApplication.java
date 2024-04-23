c85fc2afabae758e8f8026f22b75ae3f5d1a1dd9
ganhua <ganhua_18476563197@163.com>
2024-04-23 20:05:47 +0800

    初始化

---
+++ src/main/java/com/ganhua/manu/ManuBckApplication.java
@@ -0,0 +1,13 @@
+package com.ganhua.manu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManuBckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManuBckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ganhua/manu/ManuBckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganhua.manu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManuBckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
