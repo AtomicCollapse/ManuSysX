b60c228e158323689fa1c910dbe0836ef550d6a3
ganhua <ganhua_18476563197@163.com>
2024-08-16 14:53:06 +0800

    maven 打包设置

--- manux-infra/manux-infra-biz/src/test/java/top/hondaman/cloud/infra/framework/file/core/local/LocalFileClientTest.java
+++ manux-infra/manux-infra-biz/src/test/java/top/hondaman/cloud/infra/framework/file/core/local/LocalFileClientTest.java
@@ -11,16 +11,16 @@ public class LocalFileClientTest {
     @Test
     public void test(){
         // 创建客户端
-        LocalFileClientConfig config = new LocalFileClientConfig();
-        config.setDomain("http://127.0.0.1:48080");
-        config.setBasePath("/Users/yunai/file_test");
-        LocalFileClient client = new LocalFileClient(0L, config);
-        client.init();
-        // 上传文件
-        String path = IdUtil.fastSimpleUUID() + ".jpg";
-        byte[] content = ResourceUtil.readBytes("file/erweima.jpg");
-        String fullPath = client.upload(content, path, "image/jpeg");
-        System.out.println("访问地址：" + fullPath);
-        client.delete(path);
+//        LocalFileClientConfig config = new LocalFileClientConfig();
+//        config.setDomain("http://127.0.0.1:48080");
+//        config.setBasePath("/Users/yunai/file_test");
+//        LocalFileClient client = new LocalFileClient(0L, config);
+//        client.init();
+//        // 上传文件
+//        String path = IdUtil.fastSimpleUUID() + ".jpg";
+//        byte[] content = ResourceUtil.readBytes("file/erweima.jpg");
+//        String fullPath = client.upload(content, path, "image/jpeg");
+//        System.out.println("访问地址：" + fullPath);
+//        client.delete(path);
     }
 }
